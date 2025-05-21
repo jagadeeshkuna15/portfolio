8321530d37f59f7fe7bb2136b101532badc85ddd
sowmyalaveti-500apps <sowmya.laveti@500apps.com>
2025-05-21 12:10:04 -0400

    Remove node_modules from repository

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  base: '/Portfolio1/', // 🔁 Replace with your repo name
 })
